8297fb1c1005c05276f102feab84c2f4e2d51fc9
Fernando Paz <ferpaz64@gmail.com>
2024-09-02 17:16:48 -0600

    Files and Folder structure created

---
+++ src/app/shared/heavy-loaders/user-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: '<h1>Hola User Loader</h1>'
+})
+export class UserLoaderComponent {
+}
---
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [],
+  template: `<p>side-menu works!</p>`
+})
+export class SideMenuComponent {
+
+}
