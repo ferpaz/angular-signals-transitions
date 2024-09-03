222684b2ccda58e4bb6148ec0a5022faa707d8b8
Fernando Paz <ferpaz64@gmail.com>
2024-09-02 19:00:17 -0600

    Title Componente configured and added to control-flow page

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  template: `<p>Title works!</p>`
+  template: `
+    <h1
+      class="text-3xl font-bold mb-5 p-4"
+      [class]="withShadow ? 'shadow' : ''"
+    >{{ title }}</h1>
+  `
 })
 export class TitleComponent {
+
+  @Input({ required: true }) title!: string;
+
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 
 }
