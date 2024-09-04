48ac38fa2bb41b697bb520cbe180265d7a2436b9
Fernando Paz <ferpaz64@gmail.com>
2024-09-03 19:30:59 -0600

    Se implemento el Defer Options para diferntes tipos de evento On XXXXX, ojo que se puede usar When <Condition> y alli se peude poner un Signal

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
+
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
-  template: '<h1>Hola Heavy Loader Fast</h1>',
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full', cssClass]">
+      <ng-content />
+    </section>
+  `,
   styles: ``
 })
 export class HeavyLoadersFastComponent {
 
+  @Input({ required: true }) cssClass!: string;
+
+  constructor() {
+    console.log('HeavyLoadersFastComponent created');
+  }
 }
