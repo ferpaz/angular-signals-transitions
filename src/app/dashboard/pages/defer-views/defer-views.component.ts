0d83e1cee411f475d886da1887f419528298733a
Fernando Paz <ferpaz64@gmail.com>
2024-09-03 19:08:14 -0600

    Se implemento el Defer views / blocs para cargar componentes que son lentos para cargar

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
+
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './defer-views.component.html',
-  styles: ``
+  imports: [TitleComponent, HeavyLoadersSlowComponent],
+  templateUrl: './defer-views.component.html'
 })
 export default class DeferedViewsComponent {
 
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: '<h1>Hola Heavy Loader Slow</h1>',
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['h-[600px] w-full', cssClass]">
+      <h1 class="text-4xl font-bold text-center align-middle">Heavy Loaders Slow</h1>
+      <p class="text-center">This component takes 3 seconds to load</p>
+    </section>
+  `,
   styles: ``
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({ required: true }) cssClass!: string;
+
+    constructor() {
+      const start= Date.now();
+      while (Date.now() - start < 3000) {}
+
+      console.log('FINALIZO  - HeavyLoadersSlowComponent');
+    }
 
 }
