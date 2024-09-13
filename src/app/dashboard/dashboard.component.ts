c70cad41de6c053a7ca78cca2d10a29a560d35f5
Fernando Paz <ferpaz64@gmail.com>
2024-09-13 10:41:23 -0600

    Aplicación de la autenticación a la funcionalidad actual del dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MsalService } from '@azure/msal-angular';
+import { AccountInfo } from '@azure/msal-browser';
+
 import { SideMenuComponent } from '@shared/side-menu/side-menu.component';
+import { ToolbarComponent } from '@shared/toolbar/toolbar.component';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, ToolbarComponent, SideMenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
-export default class DashboardComponent {
+export default class DashboardComponent implements OnInit {
+  loginDisplay!: boolean;
 
+  private _accountInfo: AccountInfo | null = null;
+
+  private authService = inject(MsalService);
+
+  accountInfo = computed(() => this._accountInfo);
+
+  ngOnInit(): void {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+
+    if (this.loginDisplay) {
+      this._accountInfo = this.authService.instance.getActiveAccount();
+    }
+  }
 }
